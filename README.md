e2361c3d13b3309b8bf15650d8f41d96a33eec6a
dangered wolf <d@ngeredwolf.me>
2022-08-18 18:50:53 -0400

    Releasing the test under CC0

---
+++ README.md
@@ -0,0 +1,6 @@
+fxtwitter speedtest
+========
+
+A very simple node app using got that tests the speeds between fxtwitter.com, vxtwitter.com, twxtter.com, and twitter64.com to see which is the fastest
+
+CC0 Public Domain, do whatever you want with this code
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import got from 'got';
+/* Generate a random Tweet ID. This Tweet ID doesn't have to exist at all,
+   but being random is important because it means not having
+   the response already cached, so no one script has a head start. */
+let id = (Math.random() * 1555000000000000000)
 
 function benchmarkFX() {
-  // Fetch fxtwitter.com using got package and measure latency using performance.now()
   const start = performance.now();
-  got('https://fxtwitter.com/dangeredwolf/status/1560358399535202304?'+Math.random())
-    .then(response => {
+  got('https://fxtwitter.com/i/status/' + id, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)'
+    }
+  })
+    .then(() => {
       const end = performance.now();
       console.log(`fxtwitter: ${end - start}ms`);
     }).catch(error => {
@@ -13,10 +20,13 @@ function benchmarkFX() {
 }
 
 function benchmarkVX() {
-  // Fetch fxtwitter.com using got package and measure latency using performance.now()
   const start = performance.now();
-  got('https://vxtwitter.com/dangeredwolf/status/1560358399535202304?'+Math.random())
-    .then(response => {
+  got('https://vxtwitter.com/i/status/' + id, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)'
+    }
+  })
+    .then(() => {
       const end = performance.now();
       console.log(`vxtwitter: ${end - start}ms`);
     }).catch(error => {
@@ -24,5 +34,37 @@ function benchmarkVX() {
     })
 }
 
-benchmarkFX();
+function benchmarkTWX() {
+  const start = performance.now();
+  got('https://twxtter.com/i/status/' + id, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)'
+    }
+  })
+    .then(() => {
+      const end = performance.now();
+      console.log(`twxtter: ${end - start}ms`);
+    }).catch(error => {
+      console.log(error);
+    })
+}
+
+function benchmark64() {
+  const start = performance.now();
+  got('https://twitter64.com/i/status/' + id, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)'
+    }
+  })
+    .then(() => {
+      const end = performance.now();
+      console.log(`twitter64: ${end - start}ms`);
+    }).catch(error => {
+      console.log(error);
+    })
+}
+
 benchmarkVX();
+benchmarkTWX();
+benchmark64();
+benchmarkFX();
